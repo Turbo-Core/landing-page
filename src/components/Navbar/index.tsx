d31e742768a4a6028f65ab417906e9a4d0b3f6c1
Sami Yousef <mail@samiyousef.ca>
2023-03-09 02:14:18 -0500

    Initial project structure

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import style from './index.module.css'
+
+export class NavBar extends PureComponent {
+  render() {
+    return (
+      <div>NavBar</div>
+    )
+  }
+}
+
+export default NavBar
