ef61a89127518acbc66322a5e5794d8daf8b097f
Sami Yousef <mail@samiyousef.ca>
2024-03-15 03:10:55 -0400

    NocoDB changed A LOT

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-import { Api } from "nocodb-sdk";
 import validate from "deep-email-validator";
 
 import type { NextApiRequest, NextApiResponse } from "next";
@@ -42,57 +41,72 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    if (req.method !== "POST") {
-        res.status(405).json({ message: "Method not allowed" });
-        return;
-    }
-    if (!req.body) {
-        res.status(400).json({ message: "Bad request" });
-        return;
-    }
+    return new Promise(async (resolve, reject) => {
+        if (req.method !== "POST") {
+            res.status(405).json({ message: "Method not allowed" });
+            reject();
+            return;
+        }
+        if (!req.body) {
+            res.status(400).json({ message: "Bad request" });
+            reject();
+            return;
+        }
 
-    const reqBody = JSON.parse(req.body);
+        const reqBody = JSON.parse(req.body);
 
-    if (!reqBody.answers?.email?.value) {
-        res.status(400).json({ message: "Bad request" });
-        return;
-    }
+        if (!reqBody.answers?.email?.value) {
+            res.status(400).json({ message: "Bad request" });
+            reject();
+            return;
+        }
 
-    const emailRes = await validate(reqBody.answers.email.value);
+        const emailRes = await validate(reqBody.answers.email.value);
 
-    if (
-        !emailRes.validators.regex?.valid ||
-        !emailRes.validators.disposable?.valid ||
-        !emailRes.validators.mx?.valid
-    ) {
-        res.status(400).json({ message: "Please enter a valid email address" });
-        return;
-    }
+        if (
+            !emailRes.validators.regex?.valid ||
+            !emailRes.validators.disposable?.valid ||
+            !emailRes.validators.mx?.valid
+        ) {
+            res.status(400).json({
+                message: "Please enter a valid email address",
+            });
+            reject();
+            return;
+        }
 
-    dotenv.config();
-    const { AUTH_TOKEN, BASE_URL } = process.env;
+        dotenv.config();
+        const { AUTH_TOKEN, NOCO_ENDPOINT } = process.env;
 
-    if (!AUTH_TOKEN || !BASE_URL) {
-        res.status(500).json({ message: "Server error" });
-        return;
-    }
-    const api = new Api({
-        baseURL: BASE_URL,
-        headers: {
-            "xc-token": AUTH_TOKEN, // Or with xc-token
-        },
+        if (!AUTH_TOKEN || !NOCO_ENDPOINT) {
+            res.status(500).json({ message: "Server error" });
+            reject();
+            return;
+        }
+
+        ({
+            "First Name": reqBody.answers.name.value,
+            Email: reqBody.answers.email.value,
+            "Use case": reqBody.answers.usage.value.join(","),
+        });
+
+        fetch(NOCO_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "xc-auth": AUTH_TOKEN,
+            },
+            body: JSON.stringify({
+                "First Name": reqBody.answers.name.value,
+                Email: reqBody.answers.email.value,
+                "Use case": reqBody.answers.usage.value.join(","),
+            }),
+        }).then((response) => {
+            if (response.ok) res.status(200).json({ success: true });
+            else {
+                res.status(500).json({ message: "Server error" });
+                console.error(response);
+            }
+        });
     });
-
-    const DBRes = await api.dbTableRow.create("noco", "Turbocore Alpha", "Signups", {
-        "First Name": reqBody.answers.name.value,
-        "Email": reqBody.answers.email.value,
-        "Use case": reqBody.answers.usage.value.join(","),
-    });
-
-    if (DBRes.isAxiosError) {
-        res.status(500).json({ message: "Server error" });
-        return;
-    }
-
-    res.status(200).json({ success: true })
 }
